687b0dfc764e0cacf3bb4be250672ef44d63fd65
AbdulHamid-codes <abdulhamidlaspure@gmail.com>
2021-09-18 10:01:52 +0500

    Added function1 function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 	static void message() {
 		System.out.println("Hello this is a method");
 	}
+	static void function1() {
+		System.out.println("This is another method");
+	}
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
 	}
