0850293f5d82a94598e2576bb4e68b3e9ee8123f
AbdulHamid-codes <abdulhamidlaspure@gmail.com>
2021-09-18 09:58:36 +0500

    Added message function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 
 public class HelloWorld {
+	static void message() {
+		System.out.println("Hello this is a method");
+	}
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
 	}
+	
+	
 }
